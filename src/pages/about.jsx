279b1e0c26426711e24268195ceb09c56e99124f
Samantha Roman <samantharaeroman@gmail.com>
2023-11-04 11:52:35 -0400

    css work

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,12 +2,19 @@ import "./about.css";
 
 
 function About(){
-    return (
-        <div className="page about">
-            <h2>Samantha Roman</h2>
-        </div>
-    )
-}
-
+        return (
+            <div className="about">
+                <div className="about-page">
+                    <img src={'./images/owner.jpeg'} alt="Owner" />
+                    <div className="details">
+                        <h3>Hi! I'm Amy owner of The Candles Shop</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit incidunt similique nisi voluptatum amet error facere quos maxime nihil delectus pariatur adipisci minus libero voluptatibus sed eaque neque facilis dolor unde repellendus, doloribus officia labore provident modi? Saepe iste, officia nostrum eveniet temporibus adipisci deleniti sequi aspernatur, eos necessitatibus nobis, non perferendis dicta. Neque impedit voluptate ipsum voluptatem nihil illum tempora in sint rerum inventore, sit provident harum reiciendis pariatur aliquam id, maxime corrupti. Perspiciatis quasi perferendis dolore dolores at rem commodi.</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    
 
 export default About;
+
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,16 @@ import "./home.css";
 function Home(){
     return (
         <div className="home">
-            <h2>Home Page</h2>
+            <h2>Welcome to The Candles Shop</h2>
+            <div className="home-page">
+                <img src={'./images/bogo.jpeg'} alt="Owner" />
+                <div className="details">
+                    <h2>November Special</h2>
+                    <h3>Buy One Get One FREE!!!</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, nisi! Ipsa culpa repellat natus ipsum commodi cumque harum voluptate quam, alias ipsam repudiandae veritatis facilis at laborum error voluptates. Culpa amet ex nobis est nisi ducimus velit, odit dolores laboriosam omnis et perferendis voluptas pariatur aspernatur porro neque fuga iste!</p><button className="btn btn-primary"><span class="blink_me">Shop BOGO Sale</span></button>
+                </div>
+            </div>
+            
         </div>
     )
 }
